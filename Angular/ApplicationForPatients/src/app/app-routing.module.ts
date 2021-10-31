b43af1e3885f6180714ec592121e33cbddd6e46a
Nikola-Kekic <nikola.get@gmail.com>
2021-10-31 13:30:21 +0100

    feat(patientFeedback): creating homepage with sidebar  for patient, adding bootstrap in project .

--- Angular/ApplicationForPatients/src/app/app-routing.module.ts
+++ Angular/ApplicationForPatients/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { LayoutModule } from './layout/layout.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+// {
+//   path: '',
+//   component: LandingPageComponent
+// }, 
+  {
+    
+    path: 'patient',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./layout/layout.module').then(mod => mod.LayoutModule)
+      }
+    ]
+    
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Angular/ApplicationForPatients/src/app/app.module.ts
+++ Angular/ApplicationForPatients/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LayoutModule } from './layout/layout.module';
+import { ViewfeedbackComponent } from './viewfeedback/viewfeedback.component';
+//import za LandingPageComponent - sam generise
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    //LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
