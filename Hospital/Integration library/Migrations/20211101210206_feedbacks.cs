75adf70e011e2436cd3641a5eefb4e169c028356
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2021-11-01 22:10:45 +0100

    feature: connecting base to the backend

---
+++ Hospital/Integration library/Migrations/20211101210206_feedbacks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Integration_library.Migrations
+{
+    public partial class feedbacks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feedbacks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    PharmacyId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedbacks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+        }
+    }
+}
---
+++ Hospital/IntegrationAPI/Controller/FeedbacksController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Integration_library.Pharmacy.Model;
+
+namespace IntegrationAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbacksController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public FeedbacksController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Feedbacks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Feedback>>> GetFeedbacks()
+        {
+            return await _context.Feedbacks.ToListAsync();
+        }
+
+        // GET: api/Feedbacks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Feedback>> GetFeedback(int id)
+        {
+            var feedback = await _context.Feedbacks.FindAsync(id);
+
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return feedback;
+        }
+
+        // PUT: api/Feedbacks/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFeedback(int id, Feedback feedback)
+        {
+            if (id != feedback.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(feedback).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FeedbackExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Feedbacks
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Feedback>> PostFeedback(Feedback feedback)
+        {
+            _context.Feedbacks.Add(feedback);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFeedback", new { id = feedback.Id }, feedback);
+        }
+
+        // DELETE: api/Feedbacks/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Feedback>> DeleteFeedback(int id)
+        {
+            var feedback = await _context.Feedbacks.FindAsync(id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            _context.Feedbacks.Remove(feedback);
+            await _context.SaveChangesAsync();
+
+            return feedback;
+        }
+
+        private bool FeedbackExists(int id)
+        {
+            return _context.Feedbacks.Any(e => e.Id == id);
+        }
+    }
+}
