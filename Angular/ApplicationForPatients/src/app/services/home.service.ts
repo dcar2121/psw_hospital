a449f9331f3e791706c57868e9ed055643c80b21
Georgije024 <tanasic.georgije1999@gmail.com>
2021-11-01 21:52:10 +0100

    I began, something done

---
+++ Angular/ApplicationForPatients/src/app/services/home.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Feedback } from '../models/feedback';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor() { }
+
+}
