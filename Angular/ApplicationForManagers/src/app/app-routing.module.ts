971944be31eaedb4b06d6dd5fbe18f6daf304bac
Fikus00 <filipnikolic0000@gmail.com>
2021-11-06 00:09:30 +0100

    feat:Adding Hospital Map

--- Angular/ApplicationForManagers/src/app/app-routing.module.ts
+++ Angular/ApplicationForManagers/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HospitalMapComponent } from './hospital-map/hospital-map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: "", component: HospitalMapComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
