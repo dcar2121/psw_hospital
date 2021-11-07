4bd46d427a49e8c073e76a348558b8f5b682a4ec
SeekEquilibrium <markovic.ra107.2018@uns.ac.rs>
2021-11-07 07:28:12 +0100

    feat(landing_page) Added a landing page for patients

--- Angular/ApplicationForPatients/src/app/app-routing.module.ts
+++ Angular/ApplicationForPatients/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageLayoutComponent } from './homepage/homepage-layout/homepage-layout.component';
+import { LandingpageLayoutComponent } from './landingpage/landingpage-layout/landingpage-layout.component';
 
 const routes: Routes = [
-// {
-//   path: '',
-//   component: LandingPageComponent
-// }, 
+  {
+     path: 'landingpage',
+     component: LandingpageLayoutComponent
+  },
+
   {
     
     path: 'patient',
--- Angular/ApplicationForPatients/src/app/app.module.ts
+++ Angular/ApplicationForPatients/src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageLayoutComponent } from './homepage/homepage-layout/homepage-layout.component';
 import { HomepageLayoutModule } from './homepage/homepage-layout/homepage-layout.module';
+import { LandingpageLayoutModule } from './landingpage/landingpage-layout/landingpage-layout.module';
 
-//import za LandingPageComponent - sam generise
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageLayoutComponent,
-    //LandingPageComponent
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomepageLayoutModule
+    HomepageLayoutModule,
+    LandingpageLayoutModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
