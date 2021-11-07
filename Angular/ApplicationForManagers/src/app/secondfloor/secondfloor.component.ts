64b96fe9592b675d39f43a1a8a256c8122ced304
Nebojša Bogosavljev <nebojsa253@gmail.com>
2021-11-07 12:09:07 +0100

    feat: display second floor (#10)
    * feat:added 3 rooms on second floor
    
    * feat:added rest of the rooms on second floor
    
    Co-authored-by: Fikus00 <filipnikolic0000@gmail.com>

---
+++ Angular/ApplicationForManagers/src/app/secondfloor/secondfloor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondfloor',
+  templateUrl: './secondfloor.component.html',
+  styleUrls: ['./secondfloor.component.css']
+})
+export class SecondfloorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
